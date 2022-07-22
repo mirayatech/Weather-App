e1039d20a0f9a5737aa7f354df6e94c0435ee52d
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-07-22 19:08:24 +0200

    creating the layout

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,4 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faDroplet,
+  faTemperatureThreeQuarters,
+} from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   const [weather, setWeather] = useState(null);
@@ -16,7 +21,43 @@ function App() {
       });
   }, []);
 
-  return <div></div>;
+  return (
+    <div className="App">
+      {weather && (
+        <div className="weather-info">
+          <div className="header">
+            <h1>Weather App</h1>
+          </div>
+          <img src={weather.current.condition.icon} alt="weather icon" />
+          <h1>{weather.current.temp_c}°C</h1>
+          <h3>{weather.current.condition.text}</h3>
+          <h1>
+            {weather.location.name}, {weather.location.country}
+          </h1>
+          <div className="wrapper">
+            <div className="aside">
+              <FontAwesomeIcon
+                icon={faTemperatureThreeQuarters}
+                className="temprature-icon"
+              />
+              <div className="aside info">
+                <h3>{weather.current.temp_c}°C</h3>
+                <h3>Feels like</h3>
+              </div>
+            </div>
+
+            <div className="aside">
+              <FontAwesomeIcon icon={faDroplet} className="humidity-icon" />
+              <div className="aside info">
+                <h3>{weather.current.humidity}%</h3>
+                <h3>Humidity</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
